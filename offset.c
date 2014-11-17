6c8312b8c02b8a76d2f49a68a998bd7bcf6d20c3
dawnsea <keeptalk@gmail.com>
2014-11-17 10:22:16 +0900

    커널에서 자주 쓰는 구조체 접근 포인터 스킬

---
+++ offset.c
@@ -0,0 +1,11 @@
+#include "env.h"
+
+#ifndef offsetof
+	#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif	
+
+#define container_of(ptr, type, member) ({ \
+					const typeof( ((type *)0)->member ) *__mptr = (ptr); \
+					(type *)( (char *)__mptr - offsetof(type, member) );})
+					
+					
