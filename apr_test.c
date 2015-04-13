cc42e14d948036992ef3e645d498c4468fa31e82
dawnsea <keeptalk@gmail.com>
2015-04-13 13:56:28 +0900

    apr standalone

---
+++ apr_test.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "apr.h"
+#include "apr_lib.h"
+#include "apr_strings.h"
+
+#include "apr_pools.h"
+#include "apr_errno.h"
+
+
+int apr_open(void)
+{
+	apr_status_t ret;
+	
+	ret = apr_pool_initialize();
+	
+	return ret;
+	
+}
+
+
+
+void apr_close(void)
+{
+	apr_pool_terminate();
+	
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	apr_pool_t *p;
+	apr_status_t ret;
+	char *t;
+	
+	printf("hello world\n");
+	
+	ret = apr_open();
+	
+	if (ret) {
+		printf("apr_open error\n");
+		exit(1);
+	}
+	
+	ret = apr_pool_create(&p, NULL);
+	
+	if (ret) {
+		printf("pool create error\n");
+		exit(1);		
+	}
+	
+	
+	
+	t = apr_psprintf(p, "%s, %d\n", "helllllll! world!", 4885);
+	
+	printf("t = %s\n", t);
+	
+	
+	
+	apr_pool_destroy(p);
+	
+	apr_close();
+	
+	exit(0);
+}
+
